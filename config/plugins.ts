1f5e8ce020379ffd75ff5640fb61cb5d4848a330
Rangga Rifqi <rangga@ranggarifqi.com>
2023-10-29 17:05:40 +0700

    Added S3 config

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,5 +3,23 @@ export default function ({ env }) {
     seo: {
       enabled: true,
     },
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          accessKeyId: env("S3_ACCESS_KEY_ID"),
+          secretAccessKey: env("S3_ACCESS_SECRET"),
+          endpoint: env("S3_ENDPOINT"), // e.g. "s3.fr-par.scw.cloud"
+          params: {
+            Bucket: env("S3_BUCKET"),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
   };
 }
